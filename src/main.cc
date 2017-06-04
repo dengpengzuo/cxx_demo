7da11e8f61478561a5275fda296c8b3bb89c47e0
zuodengpeng <cqzuodp@163.com>
2017-06-04 20:25:53 +0800

    使用clang-format

--- src/main.cc
+++ src/main.cc
@@ -1,26 +1,26 @@
 #include "member_function_pointer.h"
 #include "template_function_object.h"
-#include "test_size.h"
-#include "test_member_addr.h"
-#include "test_construct.h"
 #include "test_2array.h"
+#include "test_construct.h"
+#include "test_member_addr.h"
+#include "test_size.h"
 
 int main(int argc, char **argv) {
-    Test_MFP::test();
-    std::cout << std::string(50,'-') << std::endl;
+  Test_MFP::test();
+  std::cout << std::string(50, '-') << std::endl;
 
-    Test_TFO::test();
-    std::cout << std::string(50,'-') << std::endl;
+  Test_TFO::test();
+  std::cout << std::string(50, '-') << std::endl;
 
-    Test_Size::test();
-    std::cout << std::string(50,'-') << std::endl;
+  Test_Size::test();
+  std::cout << std::string(50, '-') << std::endl;
 
-    Test_Member_Addr::test();
-    std::cout << std::string(50,'-') << std::endl;
+  Test_Member_Addr::test();
+  std::cout << std::string(50, '-') << std::endl;
 
-    Test_Construct::test();
-    std::cout << std::string(50,'-') << std::endl;
+  Test_Construct::test();
+  std::cout << std::string(50, '-') << std::endl;
 
-    Test_2Array::print_2array();
-    return 0;
+  Test_2Array::print_2array();
+  return 0;
 }
