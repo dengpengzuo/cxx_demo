24d73b26ddcb03a37d0057247108fe2257888a0f
zuodengpeng <cqzuodp@gmail.com>
2017-06-10 06:30:34 +0800

    使用clang-format格式化代码

--- src/template_function_object.h
+++ src/template_function_object.h
@@ -1,120 +1,111 @@
 #ifndef TEMPLATE_FUNC_H
 #define TEMPLATE_FUNC_H
 
-#include <iostream>
 #include <cstdint>
+#include <iostream>
 
 namespace Test_TFO {
-    typedef void *pvoid;
+typedef void *pvoid;
 
-    template<typename R, typename T>
-    inline static R cast_to(T *&p) {
-        return (R) (void *) (p);
+template <typename R, typename T> inline static R cast_to(T *&p) {
+  return (R)(void *)(p);
+}
+
+// ----------------------------------------------------------
+// 模板函数对象定义
+// 申明有一个 template 类 hasher，可以不用实现.
+template <typename T> struct hasher;
+
+// 标准实现是传引用进来.
+template <typename T> struct hasher {
+  // 实现操作符()函数
+  size_t operator()(T &v) {
+    std::cout << "调用的标准hasher(T &)" << std::endl;
+    T *pv = &v;
+    return cast_to<uintptr_t, T>(pv);
+  }
+};
+
+// 定义有一个 template 类 hasher.的偏特化《指针》实现.
+template <typename T> struct hasher<T *> {
+  // 实现操作符()函数
+  size_t operator()(T *&pv) {
+    std::cout << "调用的偏特化hasher(T *&)" << std::endl;
+    return cast_to<uintptr_t, T>(pv);
+  }
+};
+
+// 定义有一个 template 类 hasher.的偏特化uint32_t实现.
+template <> // 偏特化一定要有<>
+struct hasher<uint32_t> {
+  // 实现操作符()函数
+  size_t operator()(uint32_t v) {
+    std::cout << "调用的偏特化 uint32_t ()" << std::endl;
+    return v;
+  }
+};
+
+// ----------------------------------------------------------
+// @模板函数例子
+template <typename T, typename H = hasher<T>>
+size_t calc_hash(T &v, H _hasher) {
+  return _hasher(v);
+};
+
+// ----------------------------------------------------------
+// @模板对象例子
+template <typename T, typename H> struct CalcHash {
+  CalcHash() : _hasher(H()) {}
+
+  size_t calc(T &v) { return _hasher(v); }
+
+private:
+  H _hasher;
+};
+
+/*
+// ----------------------------------------------------------
+// 模板对象的偏特化
+// 偏特化时不能在template行写上初始值 如 H = hasher<T*> .
+template<typename T, typename H>
+struct CalcHash<T *, H> {
+    CalcHash() : _hasher(H()) { }
+
+    size_t calc(T *pv) {
+        return _hasher(pv);
     }
 
-    // ----------------------------------------------------------
-    // 模板函数对象定义
-    // 申明有一个 template 类 hasher，可以不用实现.
-    template<typename T>
-    struct hasher;
+private:
+    H _hasher;
+};
+*/
 
-    // 标准实现是传引用进来.
-    template<typename T>
-    struct hasher {
-        // 实现操作符()函数
-        size_t operator()(T &v) {
-            std::cout << "调用的标准hasher(T &)" << std::endl;
-            T *pv = &v;
-            return cast_to<uintptr_t, T>(pv);
-        }
-    };
+struct Item {};
 
-    // 定义有一个 template 类 hasher.的偏特化《指针》实现.
-    template<typename T>
-    struct hasher<T *> {
-        // 实现操作符()函数
-        size_t operator()(T *&pv) {
-            std::cout << "调用的偏特化hasher(T *&)" << std::endl;
-            return cast_to<uintptr_t, T>(pv);
-        }
-    };
+typedef struct Item *pitem;
 
-    // 定义有一个 template 类 hasher.的偏特化uint32_t实现.
-    template<> // 偏特化一定要有<>
-    struct hasher<uint32_t> {
-        // 实现操作符()函数
-        size_t operator()(uint32_t v) {
-            std::cout << "调用的偏特化 uint32_t ()" << std::endl;
-            return v;
-        }
-    };
+void test() {
+  uint32_t v = 10;
+  size_t hash = calc_hash<uint32_t>(v, hasher<uint32_t>());
+  std::cout << "calc_hash<uint32_t>:=" << hash << std::endl;
 
-    // ----------------------------------------------------------
-    // @模板函数例子
-    template<typename T, typename H = hasher<T> >
-    size_t calc_hash(T &v, H _hasher) {
-        return _hasher(v);
-    };
+  Item vx;
+  pitem pv = &vx;
 
-    // ----------------------------------------------------------
-    // @模板对象例子
-    template<typename T, typename H>
-    struct CalcHash {
-        CalcHash() : _hasher(H()) { }
+  // 使用 calc_hash 进行计算
+  hash = calc_hash<Item, hasher<Item>>(vx, hasher<Item>()); // 传入函数对象
+  std::cout << "calc_hash<Item, hasher<Item>>:=" << hash << std::endl;
 
-        size_t calc(T &v) {
-            return _hasher(v);
-        }
+  hash = calc_hash<pitem, hasher<pitem>>(pv, hasher<pitem>()); // 传入函数对象
+  std::cout << "calc_hash<Item*>(Item*&):=" << hash << std::endl;
 
-    private:
-        H _hasher;
-    };
+  // 使用class CalcHash 进行计算
+  hash = CalcHash<Item, hasher<Item>>().calc(vx);
+  std::cout << "CalcHash<item>.calc:=" << hash << std::endl;
 
-    /*
-    // ----------------------------------------------------------
-    // 模板对象的偏特化
-    // 偏特化时不能在template行写上初始值 如 H = hasher<T*> .
-    template<typename T, typename H>
-    struct CalcHash<T *, H> {
-        CalcHash() : _hasher(H()) { }
-
-        size_t calc(T *pv) {
-            return _hasher(pv);
-        }
-
-    private:
-        H _hasher;
-    };
-    */
-
-    struct Item {
-
-    };
-
-    typedef struct Item *pitem;
-
-    void test() {
-        uint32_t v = 10;
-        size_t hash = calc_hash<uint32_t>(v, hasher<uint32_t>());
-        std::cout << "calc_hash<uint32_t>:=" << hash << std::endl;
-
-        Item vx;
-        pitem pv = &vx;
-
-        // 使用 calc_hash 进行计算
-        hash = calc_hash<Item, hasher<Item> >(vx, hasher<Item>());             // 传入函数对象
-        std::cout << "calc_hash<Item, hasher<Item>>:=" << hash << std::endl;
-
-        hash = calc_hash<pitem, hasher<pitem> >(pv, hasher<pitem>());   // 传入函数对象
-        std::cout << "calc_hash<Item*>(Item*&):=" << hash << std::endl;
-
-        // 使用class CalcHash 进行计算
-        hash = CalcHash<Item, hasher<Item> >().calc(vx);
-        std::cout << "CalcHash<item>.calc:=" << hash << std::endl;
-
-        hash = CalcHash<pitem, hasher<pitem> >().calc(pv);
-        std::cout << "CalcHash<item*>.calc:=" << hash << std::endl;
-    }
+  hash = CalcHash<pitem, hasher<pitem>>().calc(pv);
+  std::cout << "CalcHash<item*>.calc:=" << hash << std::endl;
+}
 };
 
 #endif /* TEMPLATE_FUNC_H */
--- src/test_size.h
+++ src/test_size.h
@@ -1,94 +1,78 @@
 #ifndef TEST_SIZE_H
 #define TEST_SIZE_H
 
+#include <cstdint>
 #include <iomanip>
 #include <iostream>
 #include <string>
-#include <cstdint>
 
 namespace Test_Size {
-    class X {
-    };
+class X {};
 
-    // 64位下，vtbl指针一个，占用 8 bytes
-    // 　　　　由于有virtual 得以指针size为align，占用 8 bytes
-    //       sizeof(Y1) :=16
-    class Y1 : public virtual X {
-    public:
-        Y1(char name) : _name(name) { }
+// 64位下，vtbl指针一个，占用 8 bytes
+// 　　　　由于有virtual 得以指针size为align，占用 8 bytes
+//       sizeof(Y1) :=16
+class Y1 : public virtual X {
+public:
+  Y1(char name) : _name(name) {}
 
-        char _name;
-    };
+  char _name;
+};
 
-    // 64位下，vtbl指针一个，占用 8 bytes，自己无内部成员，直接使用[vtbl]
-    //        soizeof(Y2) := 8
-    class Y2 : public virtual X {
+// 64位下，vtbl指针一个，占用 8 bytes，自己无内部成员，直接使用[vtbl]
+//        soizeof(Y2) := 8
+class Y2 : public virtual X {};
 
-    };
+// 64位下，X无内部成员变量，自己只有一个name，所以占用1个字节
+// sizeof(Y3) := 1
+class Y3 : public X {
+public:
+  Y3(char name) : _name(name) {}
 
-    // 64位下，X无内部成员变量，自己只有一个name，所以占用1个字节
-    // sizeof(Y3) := 1
-    class Y3 : public X {
-    public:
-        Y3(char name) : _name(name) { }
+  char _name;
+};
 
-        char _name;
-    };
+// 64位下，X无变量，自己无变量，但为了区别，C++会编译占用1个字节
+// sizeof(Y4) := 1
+class Y4 : public X {};
 
-    // 64位下，X无变量，自己无变量，但为了区别，C++会编译占用1个字节
-    // sizeof(Y4) := 1
-    class Y4 : public X {
+struct PrintInfo {
+  std::string name;
+  size_t size;
 
-    };
+  PrintInfo(const char *v, size_t s) : name(v), size(s) {}
+};
 
-    struct PrintInfo {
-        std::string name;
-        size_t size;
+void printSize() {
+  struct PrintInfo info[] = {{"sizeof(int)", sizeof(int)},
+                             {"sizeof(long)", sizeof(long)},
+                             {"sizeof(size_t)", sizeof(size_t)},
+                             {"sizeof(intptr_t)", sizeof(intptr_t)},
+                             {"sizeof(X)", sizeof(X)},
+                             {"sizeof(Y1)", sizeof(Y1)},
+                             {"sizeof(Y2)", sizeof(Y2)},
+                             {"sizeof(Y3)", sizeof(Y3)},
+                             {"sizeof(Y4)", sizeof(Y4)}};
+  // {"sizeof(void)", sizeof(void)}, // TODO: g++支持有警告, clang不支持,
+  // sizeof(void)
+  for (size_t i = 0; i < sizeof(info) / sizeof(info[0]); ++i) {
+    PrintInfo *p = &info[i];
+    std::cout << std::left << std::setw(16) << p->name << " := " << std::right
+              << std::setw(2) << p->size << std::endl;
+  }
+}
 
-        PrintInfo(const char *v, size_t s) : name(v), size(s) { }
-    };
-
-    void printSize() {
-        struct PrintInfo info[] = {
-                //{"sizeof(void)", sizeof(void)}, // TODO:g++支持有警告, clang不支持, sizeof(void) return 1
-                {"sizeof(int)", sizeof(int)},
-                {"sizeof(long)", sizeof(long)},
-                {"sizeof(size_t)", sizeof(size_t)},
-                {"sizeof(intptr_t)", sizeof(intptr_t)},
-
-                {"sizeof(X)", sizeof(X)},
-                {"sizeof(Y1)", sizeof(Y1)},
-                {"sizeof(Y2)", sizeof(Y2)},
-                {"sizeof(Y3)", sizeof(Y3)},
-                {"sizeof(Y4)", sizeof(Y4)}
-        };
-
-        for (size_t i = 0; i < sizeof(info) / sizeof(info[0]); ++i) {
-            PrintInfo *p = &info[i];
-            std::cout
-            << std::left << std::setw(16) << p->name
-            << " := "
-            << std::right << std::setw(2) << p->size
+void test() {
+  uint64_t array[] = {0x0102030405060708, 0x0102030405060708};
+  std::cout << "   array[0] " << &array[0] << ", array[1] " << &array[1]
             << std::endl;
-        }
-    }
-
-    void test() {
-        uint64_t array[] = {0x0102030405060708, 0x0102030405060708};
-        std::cout << "   array[0] " << &array[0] << ", array[1] " << &array[1] << std::endl;
-        {
-            uint64_t *ptr0 = &array[0];
-            uint64_t *ptr1 = ptr0 + 1; //sizeof(long) as 8
-            std::cout << " long* ptr0 " << ptr0 << ", ptr1 " << ptr1 << std::endl;
-        }
-        /*{
-            void *ptr0 = (void *) &array[0];
-            void *ptr1 = ptr0 + 1; // sizeof(void or of a function) as 1, TODO: clang++ 会报错, arithmetic on a pointer to void
-            void *ptr1 = (void *) &array[1];
-            std::cout << " void* ptr0 " << ptr0 << ", ptr1 " << ptr1 << std::endl;
-        }*/
-        printSize();
-    }
+  {
+    uint64_t *ptr0 = &array[0];
+    uint64_t *ptr1 = ptr0 + 1; // sizeof(long) as 8
+    std::cout << " long* ptr0 " << ptr0 << ", ptr1 " << ptr1 << std::endl;
+  }
+  printSize();
+}
 };
 
 #endif // TEST_SIZE_H
