814952b7d4201380314a84d84bcc911fd7dde52e
zuodengpeng <zuodengpeng@zbj.com>
2017-06-20 12:24:39 +0800

    使用模板方式

--- src/boost/context.cc
+++ src/boost/context.cc
@@ -17,7 +17,7 @@ public:
         this->alloc_.deallocate(this->stack_);
     }
 
-    boost_uthread(Alloc const &alloc) : alloc_(alloc) {
+    boost_uthread(Alloc const &alloc = Alloc()) : alloc_(alloc) {
         this->stack_ = this->alloc_.allocate();
         this->fctx_ = boost::context::detail::make_fcontext(this->stack_.sp, this->stack_.size, &uthreadFunction);
     }
@@ -27,21 +27,21 @@ public:
         this->args_ = (args);
         // 得到协程ID，同uthreadFunction中代码，取回协程执行权.
         boost::context::detail::transfer_t t = boost::context::detail::jump_fcontext(this->fctx_, reinterpret_cast<void *>(this));
-        // fprintf(stdout, "first jump_fcontext context:%p return:%p \n", this->fctx_, t.fctx);
+        fprintf(stdout, "first jump_fcontext context:%p return:%p \n", this->fctx_, t.fctx);
         this->fctx_ = t.fctx;
     }
 
     void resume() {
         // 将执行权给协程.
         boost::context::detail::transfer_t t = boost::context::detail::jump_fcontext(this->fctx_, nullptr);
-        // fprintf(stdout, "resume jump fcontext:%p return: %p\n", this->fctx_, t.fctx);
+        fprintf(stdout, "resume jump fcontext:%p return: %p\n", this->fctx_, t.fctx);
         this->fctx_ = t.fctx;
     }
 
     void yield() {
         // 协程让出执行权
         boost::context::detail::transfer_t t = boost::context::detail::jump_fcontext(this->from_fctx_, nullptr);
-        // fprintf(stdout, "yield jump fcontext:%p return: %p\n", this->from_fctx_, t.fctx);
+        fprintf(stdout, "yield jump fcontext:%p return: %p\n", this->from_fctx_, t.fctx);
         this->from_fctx_ = t.fctx;
     }
 
@@ -95,7 +95,7 @@ void input_task(void *p) {
 }
 
 int main() {
-    pfx_ucthread c1 = pfx_ucthread(boost::context::protected_fixedsize_stack());
+    pfx_ucthread c1;
     input_args ia = {.target = 0, .count_ = 0, .quit_ = false, &c1};
     c1.run(&input_task, (void *) &ia);
 
