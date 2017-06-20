2d0b088d8a6ea51ea6fbb6d2b0c92321cab090b9
zuodengpeng <cqzuodp@163.com>
2017-06-21 05:48:25 +0800

    完善ucthread

--- src/boost/context.cc
+++ src/boost/context.cc
@@ -8,46 +8,52 @@
 
 #include <boost/context/all.hpp>
 
-typedef std::function<void(void *)> uthread_function;
+typedef std::function<void(void*)> uthread_function;
 
-template <typename Alloc >
+template <typename Alloc>
 class boost_uthread {
 public:
-    ~boost_uthread() {
+    ~boost_uthread()
+    {
         this->alloc_.deallocate(this->stack_);
     }
 
-    boost_uthread(Alloc const &alloc = Alloc()) : alloc_(alloc) {
+    boost_uthread(Alloc const& alloc = Alloc())
+        : alloc_(alloc)
+    {
         this->stack_ = this->alloc_.allocate();
-        this->fctx_ = boost::context::detail::make_fcontext(this->stack_.sp, this->stack_.size, &uthreadFunction);
     }
 
-    void run(uthread_function callback, void *args) {
+    void run(uthread_function callback, void* args)
+    {
         this->callback_ = (callback);
         this->args_ = (args);
         // 得到协程ID，同uthreadFunction中代码，取回协程执行权.
-        boost::context::detail::transfer_t t = boost::context::detail::jump_fcontext(this->fctx_, reinterpret_cast<void *>(this));
-        fprintf(stdout, "first jump_fcontext context:%p return:%p \n", this->fctx_, t.fctx);
+        auto fctx = boost::context::detail::make_fcontext(this->stack_.sp, this->stack_.size, &uthreadFunction);
+        boost::context::detail::transfer_t t = boost::context::detail::jump_fcontext(fctx, reinterpret_cast<void*>(this));
         this->fctx_ = t.fctx;
     }
 
-    void resume() {
+    void resume()
+    {
         // 将执行权给协程.
         boost::context::detail::transfer_t t = boost::context::detail::jump_fcontext(this->fctx_, nullptr);
-        fprintf(stdout, "resume jump fcontext:%p return: %p\n", this->fctx_, t.fctx);
+        // fprintf(stdout, "resume jump fcontext:%p return: %p\n", this->fctx_, t.fctx);
         this->fctx_ = t.fctx;
     }
 
-    void yield() {
+    void yield()
+    {
         // 协程让出执行权
         boost::context::detail::transfer_t t = boost::context::detail::jump_fcontext(this->from_fctx_, nullptr);
-        fprintf(stdout, "yield jump fcontext:%p return: %p\n", this->from_fctx_, t.fctx);
+        // fprintf(stdout, "yield jump fcontext:%p return: %p\n", this->from_fctx_, t.fctx);
         this->from_fctx_ = t.fctx;
     }
 
 private:
-    static void uthreadFunction(boost::context::detail::transfer_t t) {
-        boost_uthread *ptr = reinterpret_cast<boost_uthread *>(t.data);
+    static void uthreadFunction(boost::context::detail::transfer_t t)
+    {
+        boost_uthread* ptr = static_cast<boost_uthread*>(t.data);
         // return run(),放出协程执行权.
         boost::context::detail::transfer_t t_ = boost::context::detail::jump_fcontext(t.fctx, nullptr);
         ptr->from_fctx_ = t_.fctx;
@@ -58,7 +64,7 @@ private:
     boost::context::detail::fcontext_t from_fctx_;
 
     uthread_function callback_;
-    void *args_;
+    void* args_;
 
     Alloc alloc_;
     boost::context::stack_context stack_;
@@ -66,43 +72,70 @@ private:
 
 typedef boost_uthread<boost::context::protected_fixedsize_stack> pfx_ucthread;
 
-struct input_args {
+struct input_v {
     int target;
-    int count_;
     bool quit_;
-    pfx_ucthread *c_;
 };
 
-void input_task(void *p) {
-    input_args *pia = (input_args *) p;
+struct input_args {
+    input_v* in_;
+    pfx_ucthread* c_;
+};
+
+void input_task(void* p)
+{
+    input_args* pia = static_cast<input_args*>(p);
 
     while (true) {
         printf("number: ");
-        int r = scanf("%d", &(pia->target));
+        int r = scanf("%d", &(pia->in_->target));
         if (!r) {
-            pia->quit_ = true;
+            pia->in_->quit_ = true;
             // return;
         }
-        if (pia->target == 0) {
-            pia->quit_ = true;
+        if (pia->in_->target == 0) {
+            pia->in_->quit_ = true;
             // return;
         }
-        pia->count_ = pia->count_ + 1;
         pia->c_->yield();
     }
 
     printf("ERROR: should not reach the end of input function\n");
 }
 
-int main() {
-    pfx_ucthread c1;
-    input_args ia = {.target = 0, .count_ = 0, .quit_ = false, &c1};
-    c1.run(&input_task, (void *) &ia);
+struct sum_args {
+    input_v* in_;
+    pfx_ucthread* c_;
+    int count_;
+    int sum_;
+};
 
-    while (!ia.quit_) {
+void sum_task(void* p)
+{
+    sum_args* psa = static_cast<sum_args*>(p);
+    while (true) {
+        ++ (psa->count_);
+        psa->sum_ += psa->in_->target;
+        psa->c_->yield();
+    }
+
+    printf("ERROR: should not reach the end of sum function\n");
+}
+
+int main()
+{
+    pfx_ucthread c1, c2;
+    input_v in = {.target = 0, .quit_ = false };
+    input_args ia = {.in_ = &in, .c_ = &c1 };
+    sum_args sum = {.in_ = &in, .c_ = &c2, .count_ = 0, .sum_ = 0 };
+
+    c1.run(&input_task, (void*)&ia);
+    c2.run(&sum_task, (void*)&sum);
+
+    while (!in.quit_) {
         c1.resume(); // 初始是协程让出控制权，现在是让协程取得使用权.
-        // 第一次传fc_input，第二次把第一次传入的值写入，作为第二次重入的基础值.
-        fprintf(stdout, "target=%d count=%d \n", ia.target, ia.count_);
+        c2.resume(); // 初始是协程让出控制权，现在是让协程取得使用权.
+        fprintf(stdout, "input val:%d count:%d sum:%d \n", in.target, sum.count_, sum.sum_);
     }
 
     printf("main: done\n");
